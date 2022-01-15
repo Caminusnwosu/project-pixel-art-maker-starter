e8b5bff0c3ee5ea6631138c683ea94ad2feb971d
Caminusnwosu <97468672+Caminusnwosu@users.noreply.github.com>
2022-01-15 12:36:05 +0000

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,45 @@
-// Select color input
-// Select size input
+const formElem = document.querySelector("#sizePicker");
+const tableElem = document.querySelector("#pixelCanvas");
 
-// When size is submitted by the user, call makeGrid()
+formElem.addEventListener("submit", formSubmit);
+tableElem.addEventListener("click", addColor);
 
-function makeGrid() {
+function formSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(formElem);
 
-// Your code goes here!
-
+  const [width, height] = formData.entries();
+  makeGrid(width[1], height[1]);
 }
+
+function makeGrid(width, height) {
+  let grid = "";
+  for (let i = 1; i <= width; i++) {
+    grid += `<tr>`
+
+
+    for (let j = 1; j <= height; j++) {
+      grid += `<td class='cell'></td>`;
+    }
+    grid += `</tr>`;
+  }
+  tableElem.innerHTML = grid;
+}
+
+function addColor(e) {
+  const color = document.querySelector("#colorPicker").value;
+  if (e.target.classList.contains("cell")) {
+    const event = e.target;
+    console.log("berfore ask", e.target);
+    if (event.hasAttribute("style")) {
+      console.log("there is style attr to remove", e.target);
+      event.removeAttribute("style");
+    } else {
+  
+console.log("there is no att so we create it", e.target);
+
+      event.style.backgroundColor = color;
+    }
+  }
+}
+
